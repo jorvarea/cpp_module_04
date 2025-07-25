28bb7a575c64ea6876e0abcacbc8bea5c5f0004a
jorvarea <jorvarea@users.noreply.github.com>
2025-07-25 03:21:46 +0200

    add wrong animals

--- ex00/main.cpp
+++ ex00/main.cpp
@@ -1,6 +1,8 @@
 #include "Animal.hpp"
 #include "Dog.hpp"
 #include "Cat.hpp"
+#include "WrongAnimal.hpp"
+#include "WrongCat.hpp"
 #include <iostream>
 
 int main() {
@@ -9,13 +11,18 @@ int main() {
     const Animal* meta = new Animal();
     const Animal* j = new Dog();
     const Animal* i = new Cat();
+    const WrongAnimal* wrongMeta = new WrongAnimal();
+    const WrongAnimal* wrongJ = new WrongCat();
 
     std::cout << std::endl;
     std::cout << "---- Animal types ----" << std::endl;
     std::cout << "Animal type: " << meta->getType() << std::endl;
     std::cout << "Dog type: " << j->getType() << std::endl;
     std::cout << "Cat type: " << i->getType() << std::endl;
+    std::cout << "WrongAnimal type: " << wrongMeta->getType() << std::endl;
+    std::cout << "WrongCat type: " << wrongJ->getType() << std::endl;
 
+    
     std::cout << std::endl;
     std::cout << "---- Making sounds ----" << std::endl;
     std::cout << j->getType() << " makes sound: ";
@@ -26,10 +33,19 @@ int main() {
     meta->makeSound();
 
     std::cout << std::endl;
+    std::cout << "---- Making wrong sounds ----" << std::endl;
+    std::cout << wrongJ->getType() << " makes sound: ";
+    wrongJ->makeSound();
+    std::cout << wrongMeta->getType() << " makes sound: ";
+    wrongMeta->makeSound();
+
+    std::cout << std::endl;
     std::cout << "---- Deleting animals ----" << std::endl;
     delete meta;
     delete j;
     delete i;
-
+    delete wrongMeta;
+    delete wrongJ;
+    
     return 0;
 }
